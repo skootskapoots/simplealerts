02a14b49a1964526f2f336e64a3951b0d2bb6a44
skootskapoots <skootskapoots@gmail.com>
2020-07-05 16:06:33 -0500

    Added event queue utilizing requestAnimationFrame

--- src/widget.js
+++ src/widget.js
@@ -3,6 +3,7 @@ const announcementMessage = document.getElementById('announcement-message');
 const event = document.getElementById('event-type');
 const name = document.getElementById('event-name');
 
+const eventsQueue = [];
 const eventsMap = {
     follower: '{followerText}',
     subscriber: '{subscriberText}',
@@ -32,14 +33,7 @@ window.addEventListener('onEventReceived', (obj) => {
     const data = obj.detail.event;
     
     if (data.name && data.type) {
-      announcementMessage.innerHTML = eventsMap[data.type];
-      announcementContainer.classList.add('slide');
-      
-      delay({announcementDuration} * 1000 / 2)
-        .then(() => {
-          event.innerHTML = data.type;
-          name.innerHTML = data.name;
-        });
+      eventsQueue.push(data);
     }
   }
 });
@@ -47,3 +41,23 @@ window.addEventListener('onEventReceived', (obj) => {
 announcementContainer.addEventListener('animationend', () => {
   announcementContainer.classList.remove('slide');
 });
+
+const eventProcess = () => {
+  const data = eventsQueue.shift();
+
+  if (data && data.name && data.type) {
+    announcementMessage.innerHTML = eventsMap[data.type];
+    announcementContainer.classList.add('slide');
+
+    delay({announcementDuration} * 1000 / 2)
+      .then(() => {
+        event.innerHTML = data.type;
+        name.innerHTML = data.name;
+      })
+      .then(() => window.requestAnimationFrame(eventProcess));
+    } else {
+      window.requestAnimationFrame(eventProcess);
+    }
+};
+
+window.requestAnimationFrame(eventProcess);
